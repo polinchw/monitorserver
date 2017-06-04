8905db41177af3f635125e595fa1b09fdbf43966
polinchw <polinchw@netscape.net>
2017-06-04 16:45:07 -0400

    Adding rest repository.

---
+++ src/main/java/com/bpe/monitor/entity/rest/repository/AccountRepository.java
@@ -0,0 +1,12 @@
+package com.bpe.monitor.entity.rest.repository;
+
+import com.bpe.monitor.entity.Account;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.*;
+
+/**
+ * Created by polinchw on 6/4/17.
+ */
+@RepositoryRestResource(collectionResourceRel = "account", path = "account")
+public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
+}
