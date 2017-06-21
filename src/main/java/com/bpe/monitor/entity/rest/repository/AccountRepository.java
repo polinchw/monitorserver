f2ce77ce0dd7fe6529e48546d1fb9f35b25c2beb
polinchakb <polinchakb@elnk.com>
2017-06-21 10:32:04 -0400

    Adding param names.

--- src/main/java/com/bpe/monitor/entity/rest/repository/AccountRepository.java
+++ src/main/java/com/bpe/monitor/entity/rest/repository/AccountRepository.java
@@ -3,7 +3,7 @@ package com.bpe.monitor.entity.rest.repository;
 import com.bpe.monitor.entity.Account;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.*;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * Created by polinchw on 6/4/17.
