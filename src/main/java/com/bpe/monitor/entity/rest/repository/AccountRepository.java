2cf50a3d162fdc3c33ca1fc42ddfed632d18233e
polinchakb <polinchakb@elnk.com>
2017-08-01 15:10:44 -0400

    Basic auth is working.

--- src/main/java/com/bpe/monitor/entity/rest/repository/AccountRepository.java
+++ src/main/java/com/bpe/monitor/entity/rest/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.bpe.monitor.entity.rest.repository;
 
 import com.bpe.monitor.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -9,7 +10,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  * Created by polinchw on 6/4/17.
  */
 @RepositoryRestResource(collectionResourceRel = "account", path = "account")
-public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Account findByLastName(@Param("lastName") String lastName);
 
