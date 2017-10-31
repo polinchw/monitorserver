5febafd320a02d151519bdfc8ad0410fe680dfd2
polinchakb <polinchakb@elnk.com>
2017-10-31 15:51:26 -0400

    Using docker secrets just to project the db password.

--- src/main/java/com/bpe/monitor/App.java
+++ src/main/java/com/bpe/monitor/App.java
@@ -1,17 +1,31 @@
 package com.bpe.monitor;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.cars.framework.secrets.DockerSecretLoadException;
+import com.cars.framework.secrets.DockerSecrets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
 import java.util.Map;
 
 @SpringBootApplication
 public class App {
 
+    private static Logger log = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
+        loadDockerSecrets();
         SpringApplication.run(App.class, args);
+    }
+
+    private static void loadDockerSecrets() {
+        try {
+            Map<String,String> secrets = DockerSecrets.load();
+            log.info("Setting DB_PASSWORD to : "+secrets.get("DB_PASSWORD"));
+            System.setProperty("spring.datasource.password",secrets.get("DB_PASSWORD"));
+        } catch (DockerSecretLoadException e) {
+            log.warn(e.getMessage());
+        }
     }
 }
